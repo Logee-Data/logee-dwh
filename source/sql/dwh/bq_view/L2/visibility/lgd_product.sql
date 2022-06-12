eea428b8f12d826196be688297ba6af72ab096ce
Yusuf Fahry <33114786+toileto@users.noreply.github.com>
2022-06-12 19:26:25 +0700

    Change project_id

--- source/sql/dwh/bq_view/L2/visibility/lgd_product.sql
+++ source/sql/dwh/bq_view/L2/visibility/lgd_product.sql
@@ -10,7 +10,7 @@ WITH check AS (
       'product_id' AS column,
       IF(product_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_id IS NULL or product_id = ''
 
   UNION ALL
@@ -23,7 +23,7 @@ WITH check AS (
       'company_id' AS column,
       IF(company_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE company_id IS NULL or company_id = ''
 
   UNION ALL
@@ -36,7 +36,7 @@ WITH check AS (
       'product_name' AS column,
       IF(product_name IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_name IS NULL or product_name = ''
 
   UNION ALL
@@ -49,7 +49,7 @@ WITH check AS (
       'product_length' AS column,
       IF(product_length IS NULL, 'Column can not be NULL', IF(product_length = 0, 'Column can not be equal to zero', "Column can not be a negative number")) AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_length IS NULL or product_length <= 0
 
   UNION ALL
@@ -62,7 +62,7 @@ WITH check AS (
       'product_height' AS column,
       IF(product_height IS NULL, 'Column can not be NULL', IF(product_height = 0, 'Column can not be equal to zero', "Column can not be a negative number")) AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_height IS NULL or product_height <= 0
 
   UNION ALL
@@ -75,7 +75,7 @@ WITH check AS (
       'product_width' AS column,
       IF(product_width IS NULL, 'Column can not be NULL', IF(product_width = 0, 'Column can not be equal to zero', "Column can not be a negative number")) AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_width IS NULL or product_width <= 0
 
   UNION ALL
@@ -88,7 +88,7 @@ WITH check AS (
       'product_category_id' AS column,
       IF(product_category_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_category_id IS NULL or product_category_id = ''
 
   UNION ALL
@@ -101,7 +101,7 @@ WITH check AS (
       'product_sub_category_id' AS column,
       IF(product_sub_category_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_sub_category_id IS NULL or product_sub_category_id = ''
 
   UNION ALL
@@ -114,7 +114,7 @@ WITH check AS (
       'product_sub_sub_category_id' AS column,
       IF(product_sub_sub_category_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_sub_sub_category_id IS NULL or product_sub_sub_category_id = ''
 
   UNION ALL
@@ -127,7 +127,7 @@ WITH check AS (
       'brand_id' AS column,
       IF(brand_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE brand_id IS NULL or brand_id = ''
 
   UNION ALL
@@ -140,7 +140,7 @@ WITH check AS (
       'product_description' AS column,
       IF(product_description IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_description IS NULL or product_description = ''
 
   UNION ALL
@@ -153,7 +153,7 @@ WITH check AS (
       'product_weight' AS column,
       IF(product_weight IS NULL, 'Column can not be NULL', IF(product_weight = 0, 'Column can not be equal to zero', "Column can not be a negative number")) AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_weight IS NULL or product_weight <= 0
 
   UNION ALL
@@ -166,7 +166,7 @@ WITH check AS (
       'product_price' AS column,
       IF(product_price IS NULL, 'Column can not be NULL', IF(product_price = 0, 'Column can not be equal to zero', "Column can not be a negative number")) AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_price IS NULL or product_price <= 0
 
   UNION ALL
@@ -179,7 +179,7 @@ WITH check AS (
       'product_stock' AS column,
       IF(product_stock IS NULL, 'Column can not be NULL', 'Column can not be a negative number') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_stock IS NULL or product_stock < 0
 
   UNION ALL
@@ -192,7 +192,7 @@ WITH check AS (
       'product_minimum_order' AS column,
       IF(product_minimum_order IS NULL, 'Column can not be NULL', 'Column can not be a negative number') AS quality_notes
     ) AS quality_check
-  FROM `logee-data-dev.L1_visibility.lgd_product`
+  FROM `logee-data-prod.L1_visibility.lgd_product`
   WHERE product_minimum_order IS NULL or product_minimum_order < 0
 
 )
@@ -212,7 +212,7 @@ SELECT
   A.*,
   B.quality_check
 FROM
-  `logee-data-dev.L1_visibility.lgd_product` A
+  `logee-data-prod.L1_visibility.lgd_product` A
   LEFT JOIN aggregated_check B
   ON A.original_data = B.original_data
   AND A.published_timestamp = B.published_timestamp
