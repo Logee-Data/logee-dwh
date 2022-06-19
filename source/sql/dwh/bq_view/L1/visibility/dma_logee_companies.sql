d2e72a5fbe8592ac9980a54f7941a2b6bc950d2f
meliskapurnama <107382939+meliskapurnama@users.noreply.github.com>
2022-06-19 12:16:33 +0700

    Update dma_logee_companies.sql
    fix issues

--- source/sql/dwh/bq_view/L1/visibility/dma_logee_companies.sql
+++ source/sql/dwh/bq_view/L1/visibility/dma_logee_companies.sql
@@ -11,7 +11,7 @@ base AS (
     data,
     ts AS published_timestamp,
     ARRAY_AGG (
-      REPLACE(allowed_payment_type, '"', '')
+      DISTINCT REPLACE(allowed_payment_type, '"', '')
     ) AS allowed_payment_type
   FROM
     base,
@@ -27,7 +27,7 @@ base AS (
     data,
     ts AS published_timestamp,
     ARRAY_AGG (
-      REPLACE(partner_ids, '"', '')
+      DISTINCT REPLACE(partner_ids, '"', '')
     ) AS partner_ids
   FROM
     base,
@@ -103,60 +103,60 @@ base AS (
 SELECT 
   REPLACE(JSON_EXTRACT(A.data, '$.companyId'), '"', '') AS company_id,
   REPLACE(JSON_EXTRACT(A.data, '$.name'), '"', '') AS name,
-  REPLACE(JSON_EXTRACT(A.data, '$.type'), '"', '') AS type,
+  IF(REPLACE(JSON_EXTRACT(A.data, '$.type'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(A.data, '$.type'), '"', '')) AS type,
   REPLACE(JSON_EXTRACT(A.data, '$.address'), '"', '') AS address,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.cityId'), '"', '') AS INT64) AS cityId,
+  REPLACE(JSON_EXTRACT(A.data, '$.cityId'), '"', '') AS city_id,
   REPLACE(JSON_EXTRACT(A.data, '$.cityName'), '"', '') AS city_name,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.province_id'), '"', '') AS INT64) AS province_id,
+  REPLACE(JSON_EXTRACT(A.data, '$.provinceId'), '"', '') AS province_id,
   REPLACE(JSON_EXTRACT(A.data, '$.provinceName'), '"', '') AS province_name,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.district_id'), '"', '') AS INT64) AS district_id,
+  REPLACE(JSON_EXTRACT(A.data, '$.districtId'), '"', '') AS district_id,
   REPLACE(JSON_EXTRACT(A.data, '$.districtName'), '"', '') AS district_name,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.subdistrictId'), '"', '') AS INT64) AS sub_district_id,
+  REPLACE(JSON_EXTRACT(A.data, '$.subdistrictId'), '"', '')AS sub_district_id,
   REPLACE(JSON_EXTRACT(A.data, '$.subdistrictName'), '"', '') AS sub_district_name,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.zipCode'), '"', '') AS INT64) AS zip_code,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.phone'), '"', '') AS INT64) AS phone,
+  REPLACE(JSON_EXTRACT(A.data, '$.zipCode'), '"', '') AS zip_code,
+  REPLACE(JSON_EXTRACT(A.data, '$.phone'), '"', '') AS phone,
   REPLACE(JSON_EXTRACT(A.data, '$.email'), '"', '') AS email,
   IF(REPLACE(JSON_EXTRACT(A.data, '$.website'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(A.data, '$.website'), '"', '')) AS website,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.npwpNum'), '"', '') AS INT64) AS npwp_num,
+  REPLACE(JSON_EXTRACT(A.data, '$.npwpNum'), '"', '') AS npwp_num,
   REPLACE(JSON_EXTRACT(A.data, '$.siupNum'), '"', '') AS siup_num,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.siupExpiryDate'), '"', '') AS TIMESTAMP) AS siup_expiry_date,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.directorKtpNum'), '"', '') AS INT64) AS director_ktp_num,
+  DATE(REPLACE(JSON_EXTRACT(A.data, '$.siupExpiryDate'), '"', '')) AS siup_expiry_date,
+  REPLACE(JSON_EXTRACT(A.data, '$.directorKtpNum'), '"', '') AS director_ktp_num,
   REPLACE(JSON_EXTRACT(A.data, '$.directorName'), '"', '') AS director_name,
   CAST(REPLACE(JSON_EXTRACT(A.data, '$.creditLimit'), '"', '') AS FLOAT64) AS credit_Limit,
   REPLACE(JSON_EXTRACT(A.data, '$.partnershipType'), '"', '') AS partnership_type,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.latitude'), '"', '') AS INT64) AS latitude,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.longitude'), '"', '') AS INT64) AS longitude,
-  REPLACE(JSON_EXTRACT(A.data, '$.pksNum'), '"', '') AS pks_num,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.pksExpiryDate'), '"', '') AS TIMESTAMP) AS pks_expiry_date,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.availableCredit'), '"', '') AS FLOAT64) AS available_credit,
-  REPLACE(JSON_EXTRACT(A.data, '$.mandiriInvoiceId'), '"', '') AS mandiri_invoice_id,
-  REPLACE(JSON_EXTRACT(A.data, '$.avatar'), '"', '') AS avatar,
-  REPLACE(JSON_EXTRACT(A.data, '$.form'), '"', '') AS form,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.dueDate'), '"', '') AS INT64) AS due_date,
-  REPLACE(JSON_EXTRACT(A.data, '$.paymentType'), '"', '') AS payment_type,
-  REPLACE(JSON_EXTRACT(A.data, '$.bankName'), '"', '') AS bank_name,
-  REPLACE(JSON_EXTRACT(A.data, '$.bankAccountName'), '"', '') AS bank_account_name,
-  REPLACE(JSON_EXTRACT(A.data, '$.bankAccountNum'), '"', '') AS bank_account_num,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.bankTransferCode'), '"', '') AS INT64) AS bank_transfer_code,
-  REPLACE(JSON_EXTRACT(A.data, '$.feePercentage'), '"', '') AS fee_percentage,
   STRUCT(
     REPLACE(JSON_EXTRACT(A.data, '$.companyDocument.npwpFile'), '"', '') AS npwp_file,
     REPLACE(JSON_EXTRACT(A.data, '$.companyDocument.siupFile'), '"', '') AS siup_file,
     REPLACE(JSON_EXTRACT(A.data, '$.companyDocument.directorKtpFile'), '"', '') AS director_ktp_file
   ) AS company_document,
+  CAST(REPLACE(JSON_EXTRACT(A.data, '$.latitude'), '"', '') AS FLOAT64) AS latitude,
+  CAST(REPLACE(JSON_EXTRACT(A.data, '$.longitude'), '"', '') AS FLOAT64) AS longitude,
+  B.partner_ids,
+  IF(REPLACE(JSON_EXTRACT(A.data, '$.pksNum'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(A.data, '$.pksNum'), '"', '')) AS pks_num,
+  DATE(REPLACE(JSON_EXTRACT(A.data, '$.pksExpiryDate'), '"', '')) AS pks_expiry_date,
+  CAST(REPLACE(JSON_EXTRACT(A.data, '$.availableCredit'), '"', '') AS FLOAT64) AS available_credit,
+  REPLACE(JSON_EXTRACT(A.data, '$.mandiriInvoiceId'), '"', '') AS mandiri_invoice_id,
+   C.allowed_payment_type,
+  REPLACE(JSON_EXTRACT(A.data, '$.avatar'), '"', '') AS avatar,
+  REPLACE(JSON_EXTRACT(A.data, '$.form'), '"', '') AS form,
+  REPLACE(JSON_EXTRACT(A.data, '$.dueDate'), '"', '') AS due_date,
+  IF(REPLACE(JSON_EXTRACT(A.data, '$.paymentType'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(A.data, '$.paymentType'), '"', '')) AS payment_type,
+  IF(REPLACE(JSON_EXTRACT(A.data, '$.bankName'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(A.data, '$.bankName'), '"', '')) AS bank_name,
+  REPLACE(JSON_EXTRACT(A.data, '$.bankTransferCode'), '"', '') AS bank_transfer_code,
+  CAST(REPLACE(JSON_EXTRACT(A.data, '$.feePercentage'), '"', '') AS FLOAT64) AS fee_percentage,
+  IF(REPLACE(JSON_EXTRACT(A.data, '$.salesId'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(A.data, '$.salesId'), '"', '')) AS sales_id,
+  IF(REPLACE(JSON_EXTRACT(A.data, '$.salesName'), '"', '') = "-", NULL, REPLACE(JSON_EXTRACT(A.data, '$.salesName'), '"', '')) AS sales_name,
+  CAST(JSON_EXTRACT(A.data, '$.isActive') AS BOOLEAN) AS is_active,
+  CAST(JSON_EXTRACT(A.data, '$.isDeleted') AS BOOLEAN) AS is_deleted,
+  REPLACE(JSON_EXTRACT(A.data, '$.createdBy'), '"', '') AS created_by,
+  CAST(REPLACE(JSON_EXTRACT(A.data, '$.createdAt'), '"', '') AS TIMESTAMP) AS created_at,
+  REPLACE(JSON_EXTRACT(A.data, '$.modifiedBy'), '"', '') AS modified_by,
+  CAST(REPLACE(JSON_EXTRACT(A.data, '$.modifiedAt'), '"', '') AS TIMESTAMP) AS modified_at,
+  IF(REPLACE(JSON_EXTRACT(A.data, '$.bankAccountName'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(A.data, '$.bankAccountName'), '"', '')) AS bank_account_name,
+  REPLACE(JSON_EXTRACT(A.data, '$.bankAccountNum'), '"', '') AS bank_account_num,
   REPLACE(JSON_EXTRACT(A.data, '$.tracking'), '"', '') AS tracking,
   E.visibility,
   F.control,
-  B.partner_ids,
-  C.allowed_payment_type,
-  CAST(JSON_EXTRACT(A.data, '$.isActive') AS BOOLEAN) AS is_active,
-  CAST(JSON_EXTRACT(A.data, '$.isDeleted') AS BOOLEAN) AS is_deleted,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.createdAt'), '"', '') AS TIMESTAMP) AS created_at,
-  REPLACE(JSON_EXTRACT(A.data, '$.createdBy'), '"', '') AS created_by,
-  CAST(REPLACE(JSON_EXTRACT(A.data, '$.modifiedAt'), '"', '') AS TIMESTAMP) AS modified_at,
-  REPLACE(JSON_EXTRACT(A.data, '$.modifiedBy'), '"', '') AS modified_by,
-  IF(REPLACE(JSON_EXTRACT(A.data, '$.salesId'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(A.data, '$.salesId'), '"', '')) AS sales_id,
-  IF(REPLACE(JSON_EXTRACT(A.data, '$.salesName'), '"', '') = "-", NULL, REPLACE(JSON_EXTRACT(A.data, '$.salesName'), '"', '')) AS sales_name,
   A.data AS original_data,
   ts AS published_timestamp
 FROM base A
@@ -174,4 +174,4 @@ FROM base A
 
   LEFT JOIN control F
   ON A.data = F.data
-  AND A.ts = F.published_timestamp
+  AND A.ts = F.published_timestamp 
