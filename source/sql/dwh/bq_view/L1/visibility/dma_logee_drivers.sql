df7c20ca7ed8720c0c522ed08c8416e1c4532642
Rendi Kustiawan <rendikstwn27992gmail.com>
2022-06-21 09:57:25 +0700

    Update dma_logee_drivers

--- source/sql/dwh/bq_view/L1/visibility/dma_logee_drivers.sql
+++ source/sql/dwh/bq_view/L1/visibility/dma_logee_drivers.sql
@@ -2,7 +2,7 @@ WITH BASE AS (
   SELECT
     *
   FROM
-    `logee-data-prod.logee_datalake_raw_development.visibility_dma_logee_drivers`
+    logee-data-prod.logee_datalake_raw_production.visibility_dma_logee_drivers
   WHERE _date_partition >= "2022-06-08"
 )
 
@@ -10,8 +10,7 @@ SELECT
   IF(REPLACE(JSON_EXTRACT(data, '$.driverId'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.driverId'), '"', ''))  AS driver_id,
   IF(REPLACE(JSON_EXTRACT(data, '$.name'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.name'), '"', ''))  AS name,
   IF(REPLACE(JSON_EXTRACT(data, '$.phone'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.phone'), '"', ''))  AS phone,
-  IF(REPLACE(JSON_EXTRACT(data, '$.email'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.email'), '"', ''))  AS email_id,
-  REPLACE(JSON_EXTRACT(data, '$.email'), '"', '') AS email,
+  IF(REPLACE(JSON_EXTRACT(data, '$.email'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.email'), '"', ''))  AS email,
   REPLACE(JSON_EXTRACT(data, '$.placeOfBirth'), '"', '') AS place_of_birth,
   CAST(IF(REPLACE(JSON_EXTRACT(data, '$.dateOfBirth'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.dateOfBirth'), '"', '')) AS TIMESTAMP) AS date_of_birth,
   REPLACE(JSON_EXTRACT(data, '$.ktpNum'), '"', '') AS ktp_num,
