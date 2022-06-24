4d1a449acc7897dfa5090238a712d3beb6aae6b5
meliskapurnama <107382939+meliskapurnama@users.noreply.github.com>
2022-06-24 21:51:00 +0700

    Update lgd_voucher.sql

--- source/sql/dwh/bq_view/L2/visibility/lgd_voucher.sql
+++ source/sql/dwh/bq_view/L2/visibility/lgd_voucher.sql
@@ -77,6 +77,18 @@ WITH check AS (
   ) AS quality_check
   FROM `logee-data-prod.L1_visibility.lgd_voucher` 
   WHERE voucher_status IS NULL or voucher_status = ''
+  
+  UNION ALL
+  
+  SELECT
+    original_data,
+    published_timestamp,
+    STRUCT(
+      'is_deleted' AS column,
+      IF(is_deleted IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
+    ) AS quality_check
+  FROM `logee-data-prod.L1_visibility.lgd_voucher` 
+  WHERE is_deleted IS NULL
 )
 
 
