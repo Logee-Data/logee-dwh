2fec6df5a3d96534ce4830a18d47a5f6bcac4e7d
Yusuf Fahry <33114786+toileto@users.noreply.github.com>
2022-07-01 11:33:17 +0700

    Init dma_logee_drivers L1

--- dags/source/sql/dwh/L1/visibility/dma_logee_drivers.sql
+++ dags/source/sql/dwh/L1/visibility/dma_logee_drivers.sql
@@ -3,27 +3,29 @@ WITH BASE AS (
     *
   FROM
     `logee-data-prod.logee_datalake_raw_production.visibility_dma_logee_drivers`
-  WHERE _date_partition >= "2022-01-01"
+  WHERE
+   _date_partition IN ('{{ ds }}', '{{ next_ds }}')
+    AND ts BETWEEN '{{ execution_date }}' AND '{{ next_execution_date }}'
 )
 
 SELECT
-  IF(REPLACE(JSON_EXTRACT(data, '$.driverId'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.driverId'), '"', ''))  AS driver_id,
-  IF(REPLACE(JSON_EXTRACT(data, '$.name'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.name'), '"', ''))  AS name,
-  IF(REPLACE(JSON_EXTRACT(data, '$.phone'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.phone'), '"', ''))  AS phone,
-  IF(REPLACE(JSON_EXTRACT(data, '$.email'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.email'), '"', ''))  AS email,
-  REPLACE(JSON_EXTRACT(data, '$.placeOfBirth'), '"', '') AS place_of_birth,
-  DATE(IF(REPLACE(JSON_EXTRACT(data, '$.dateOfBirth'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.dateOfBirth'), '"', ''))) AS date_of_birth,
-  REPLACE(JSON_EXTRACT(data, '$.ktpNum'), '"', '') AS ktp_num,
-  REPLACE(JSON_EXTRACT(data, '$.simType'), '"', '') AS sim_type,
-  REPLACE(JSON_EXTRACT(data, '$.simNum'), '"', '') AS sim_num,
-  REPLACE(JSON_EXTRACT(data, '$.address'), '"', '') AS address,
-  REPLACE(JSON_EXTRACT(data, '$.companyId'), '"', '') AS company_id,
-  REPLACE(JSON_EXTRACT(data, '$.companyName'), '"', '') AS company_name,
-  IF(IF(REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', '')) NOT LIKE '+%',
-     DATE(IF(REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', ''))),
-     NULL 
-  ) AS sim_expired,
-  data AS original_data,
-  ts AS published_timestamp
+    IF(REPLACE(JSON_EXTRACT(data, '$.driverId'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.driverId'), '"', ''))  AS driver_id,
+    IF(REPLACE(JSON_EXTRACT(data, '$.name'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.name'), '"', ''))  AS name,
+    IF(REPLACE(JSON_EXTRACT(data, '$.phone'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.phone'), '"', ''))  AS phone,
+    IF(REPLACE(JSON_EXTRACT(data, '$.email'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(data, '$.email'), '"', ''))  AS email,
+    REPLACE(JSON_EXTRACT(data, '$.placeOfBirth'), '"', '') AS place_of_birth,
+    DATE(IF(REPLACE(JSON_EXTRACT(data, '$.dateOfBirth'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.dateOfBirth'), '"', ''))) AS date_of_birth,
+    REPLACE(JSON_EXTRACT(data, '$.ktpNum'), '"', '') AS ktp_num,
+    REPLACE(JSON_EXTRACT(data, '$.simType'), '"', '') AS sim_type,
+    REPLACE(JSON_EXTRACT(data, '$.simNum'), '"', '') AS sim_num,
+    REPLACE(JSON_EXTRACT(data, '$.address'), '"', '') AS address,
+    REPLACE(JSON_EXTRACT(data, '$.companyId'), '"', '') AS company_id,
+    REPLACE(JSON_EXTRACT(data, '$.companyName'), '"', '') AS company_name,
+    IF(IF(REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', '')) NOT LIKE '+%',
+        DATE(IF(REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', ''))),
+        NULL
+    ) AS sim_expired,
+    CAST(ts AS TIMESTAMP) AS modified_at,
+    CAST(ts AS TIMESTAMP) AS published_timestamp
 FROM
   BASE
