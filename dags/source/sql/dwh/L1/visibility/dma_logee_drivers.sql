5ed7424a0dae1ebe8f0ebf66486fee471b74a19b
Fikri Haykal <fikri.haykal@telkom.co.id>
2022-07-08 00:52:54 +0700

    add some new columns

--- dags/source/sql/dwh/L1/visibility/dma_logee_drivers.sql
+++ dags/source/sql/dwh/L1/visibility/dma_logee_drivers.sql
@@ -21,11 +21,20 @@ SELECT
   IF(REPLACE(JSON_EXTRACT(data, '$.address'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.address'), '"', '')) AS address,
   IF(REPLACE(JSON_EXTRACT(data, '$.companyId'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.companyId'), '"', '')) AS company_id,
   IF(REPLACE(JSON_EXTRACT(data, '$.companyName'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.companyName'), '"', '')) AS company_name,
+  IF(REPLACE(JSON_EXTRACT(data, '$.imageSim'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.imageSim'), '"', '')) AS image_sim,
+  IF(REPLACE(JSON_EXTRACT(data, '$.imageKtp'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.imageKtp'), '"', '')) AS image_ktp,
+  CAST(IF(REPLACE(JSON_EXTRACT(data, '$.driverStatus'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.driverStatus'), '"', '')) AS BOOL) AS driver_status,
+  IF(REPLACE(JSON_EXTRACT(data, '$.avatar'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.avatar'), '"', '')) AS avatar,
+  CAST(IF(REPLACE(JSON_EXTRACT(data, '$.isActive'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.isActive'), '"', '')) AS BOOL) AS is_active,
+  CAST(IF(REPLACE(JSON_EXTRACT(data, '$.isDeleted'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.isDeleted'), '"', '')) AS BOOL) AS is_deleted,
   IF(IF(REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', '')) NOT LIKE '+%',
       DATE(IF(REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.simExpiryDate'), '"', ''))),
       NULL
   ) AS sim_expired,
-  CAST(ts AS TIMESTAMP) AS modified_at,
+  CAST(IF(REPLACE(JSON_EXTRACT(data, '$.createdAt'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.createdAt'), '"', '')) AS TIMESTAMP) AS created_at,
+  IF(REPLACE(JSON_EXTRACT(data, '$.createdBy'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.createdBy'), '"', '')) AS created_by,
+  CAST(IF(REPLACE(JSON_EXTRACT(data, '$.modifiedAt'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.modifiedAt'), '"', '')) AS TIMESTAMP) AS modified_at,
+  IF(REPLACE(JSON_EXTRACT(data, '$.modifiedBy'), '"', '') = '', NULL, REPLACE(JSON_EXTRACT(data, '$.modifiedBy'), '"', '')) AS modified_by,
   CAST(ts AS TIMESTAMP) AS published_timestamp
 FROM
   base
