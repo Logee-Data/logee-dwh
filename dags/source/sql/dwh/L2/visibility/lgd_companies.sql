bda9d98e0d5870984a261012d3fc12c59e6cb230
Fikri Haykal <fikri.haykal@telkom.co.id>
2022-07-12 12:04:57 +0700

    uncomment executiion date

--- dags/source/sql/dwh/L2/visibility/lgd_companies.sql
+++ dags/source/sql/dwh/L2/visibility/lgd_companies.sql
@@ -13,8 +13,8 @@ WITH base as(
     ) AS bank_account
     )
   FROM `logee-data-prod.L1_visibility.lgd_companies`
-  -- WHERE
-  --   modified_at BETWEEN '{{ execution_date }}' AND '{{ next_execution_date }}'
+  WHERE
+     modified_at BETWEEN '{{ execution_date }}' AND '{{ next_execution_date }}'
 )
 
 
@@ -395,7 +395,6 @@ WITH base as(
 
 SELECT
   *
-  -- ,
   -- B.quality_check
 FROM 
   base 
