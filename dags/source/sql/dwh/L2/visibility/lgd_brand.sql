93bfc30081eb0301076749f9a23995057a44b0dc
Fikri Haykal <fikri.haykal@telkom.co.id>
2022-07-12 11:10:38 +0700

    disable qc

--- dags/source/sql/dwh/L2/visibility/lgd_brand.sql
+++ dags/source/sql/dwh/L2/visibility/lgd_brand.sql
@@ -8,51 +8,51 @@ WITH base AS (
 )
 
 -- BEGIN CHECK
-,check AS (
-  SELECT
-    brand_id,
-    published_timestamp,
-    STRUCT(
-      'brand_id' AS column,
-      IF(brand_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
-    ) AS quality_check
-  FROM
-    base
-  WHERE
-    brand_id IS NULL or brand_id = ''
+-- ,check AS (
+--   SELECT
+--     brand_id,
+--     published_timestamp,
+--     STRUCT(
+--       'brand_id' AS column,
+--       IF(brand_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
+--     ) AS quality_check
+--   FROM
+--     base
+--   WHERE
+--     brand_id IS NULL or brand_id = ''
   
-  UNION ALL
+--   UNION ALL
 
-  SELECT
-    brand_id,
-    published_timestamp,
-    STRUCT(
-      'brand_name' AS column,
-      IF(brand_name IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
-    ) AS quality_check
-  FROM
-    base
-  WHERE
-    brand_name IS NULL or brand_name = ''
-)
+--   SELECT
+--     brand_id,
+--     published_timestamp,
+--     STRUCT(
+--       'brand_name' AS column,
+--       IF(brand_name IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
+--     ) AS quality_check
+--   FROM
+--     base
+--   WHERE
+--     brand_name IS NULL or brand_name = ''
+-- )
 -- END CHECK
 
-,aggregated_check AS (
-  SELECT
-    brand_id,
-    published_timestamp,
-    ARRAY_AGG(
-      quality_check
-    ) AS quality_check
-  FROM check
-  GROUP BY 1,2
-)
+-- ,aggregated_check AS (
+--   SELECT
+--     brand_id,
+--     published_timestamp,
+--     ARRAY_AGG(
+--       quality_check
+--     ) AS quality_check
+--   FROM check
+--   GROUP BY 1,2
+-- )
 
 SELECT
   A.*,
-  B.quality_check
+  -- B.quality_check
 FROM
   base A
-  LEFT JOIN aggregated_check B
-  ON A.brand_id = B.brand_id
-  AND A.published_timestamp = B.published_timestamp
+  -- LEFT JOIN aggregated_check B
+  -- ON A.brand_id = B.brand_id
+  -- AND A.published_timestamp = B.published_timestamp
