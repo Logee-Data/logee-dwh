007ec4204f9909a077c86bd1a82cf3713dec06a1
Fikri Haykal <fikri.haykal@telkom.co.id>
2022-07-12 11:12:12 +0700

    disable qc

--- dags/source/sql/dwh/L2/visibility/lgd_category.sql
+++ dags/source/sql/dwh/L2/visibility/lgd_category.sql
@@ -8,107 +8,107 @@ WITH base AS (
 )
 
 -- BEGIN CHECK
-,check AS (
-  SELECT
-    category_id,
-    published_timestamp,
-    STRUCT(
-      'category_id' AS column,
-      IF(category_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
-    ) AS quality_check
-  FROM
-    base
-  WHERE
-    category_id IS NULL or category_id = ''
+-- ,check AS (
+--   SELECT
+--     category_id,
+--     published_timestamp,
+--     STRUCT(
+--       'category_id' AS column,
+--       IF(category_id IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
+--     ) AS quality_check
+--   FROM
+--     base
+--   WHERE
+--     category_id IS NULL or category_id = ''
   
-  UNION ALL
+--   UNION ALL
 
-  SELECT
-    category_id,
-    published_timestamp,
-    STRUCT(
-      'category_name' AS column,
-      IF(category_name IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
-    ) AS quality_check
-  FROM
-    base
-  WHERE
-    category_name IS NULL or category_name = ''
+--   SELECT
+--     category_id,
+--     published_timestamp,
+--     STRUCT(
+--       'category_name' AS column,
+--       IF(category_name IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
+--     ) AS quality_check
+--   FROM
+--     base
+--   WHERE
+--     category_name IS NULL or category_name = ''
   
-  UNION ALL
+--   UNION ALL
 
-  SELECT
-    category_id,
-    published_timestamp,
-    STRUCT(
-      'category_image' AS column,
-      IF(category_image	 IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
-    ) AS quality_check
-  FROM
-    base
-  WHERE
-    category_image IS NULL or category_image = ''
+--   SELECT
+--     category_id,
+--     published_timestamp,
+--     STRUCT(
+--       'category_image' AS column,
+--       IF(category_image	 IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
+--     ) AS quality_check
+--   FROM
+--     base
+--   WHERE
+--     category_image IS NULL or category_image = ''
   
-  UNION ALL
+--   UNION ALL
 
-  SELECT
-    category_id,
-    published_timestamp,
-    STRUCT(
-      'category_type' AS column,
-      IF(category_type	 IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
-    ) AS quality_check
-  FROM
-    base
-  WHERE
-    category_type IS NULL or category_type = ''
+--   SELECT
+--     category_id,
+--     published_timestamp,
+--     STRUCT(
+--       'category_type' AS column,
+--       IF(category_type	 IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
+--     ) AS quality_check
+--   FROM
+--     base
+--   WHERE
+--     category_type IS NULL or category_type = ''
   
-  UNION ALL
+--   UNION ALL
 
-  SELECT
-    category_id,
-    published_timestamp,
-    STRUCT(
-      'total_product_bind' AS column,
-      IF(total_product_bind IS NULL, 'Column can not be NULL', IF(total_product_bind = 0, 'Column can not be equal to zero', "Column can not be a negative number")) AS quality_notes
-    ) AS quality_check
-  FROM
-    base
-  WHERE
-    total_product_bind IS NULL or total_product_bind <= 0
+--   SELECT
+--     category_id,
+--     published_timestamp,
+--     STRUCT(
+--       'total_product_bind' AS column,
+--       IF(total_product_bind IS NULL, 'Column can not be NULL', IF(total_product_bind = 0, 'Column can not be equal to zero', "Column can not be a negative number")) AS quality_notes
+--     ) AS quality_check
+--   FROM
+--     base
+--   WHERE
+--     total_product_bind IS NULL or total_product_bind <= 0
 
-  UNION ALL
+--   UNION ALL
 
-  SELECT
-    category_id,
-    published_timestamp,
-    STRUCT(
-      'parent_id' AS column,
-      IF(category_type	 IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
-    ) AS quality_check
-  FROM
-    base
-  WHERE
-    parent_id IS NULL or parent_id = ''
-)
+--   SELECT
+--     category_id,
+--     published_timestamp,
+--     STRUCT(
+--       'parent_id' AS column,
+--       IF(category_type	 IS NULL, 'Column can not be NULL', 'Column can not be an empty string') AS quality_notes
+--     ) AS quality_check
+--   FROM
+--     base
+--   WHERE
+--     parent_id IS NULL or parent_id = ''
+-- )
 -- END CHECK
 
-,aggregated_check AS (
-  SELECT 
-    category_id,
-    published_timestamp,
-    ARRAY_AGG(
-      quality_check
-    ) AS quality_check
-  FROM check
-  GROUP BY 1, 2
-)
+-- ,aggregated_check AS (
+--   SELECT 
+--     category_id,
+--     published_timestamp,
+--     ARRAY_AGG(
+--       quality_check
+--     ) AS quality_check
+--   FROM check
+--   GROUP BY 1, 2
+-- )
 
 SELECT
-  A.*,
-  B.quality_check
+  A.*
+  -- B.quality_check
 FROM
   base A
-  LEFT JOIN aggregated_check B
-  ON A.category_id = B.category_id
-  AND A.published_timestamp = B.published_timestamp
+  -- LEFT JOIN aggregated_check B
+  -- ON A.category_id = B.category_id
+  -- AND A.published_timestamp = B.published_timestamp
