e963414a1bfaab4914cffd266c765651c31a7a8f
madityaar <m.adityarayhan@yahoo.co.id>
2022-07-24 23:45:02 +0700

    first push for required sql and yaml lgd_general

---
+++ dags/source/sql/dwh/L1/lgd_general/store_cirebon.sql
@@ -0,0 +1,22 @@
+with base as (SELECT
+  szCustId AS customer_id,
+  szName AS customer_name,
+  CustszContactPerson AS customer_contact_person,
+  CustszPhoneNo_1 as customer_contact_number,
+  CustszEmail as customer_email,
+  INITCAP(CustszBuilding) AS customer_building,
+  INITCAP(CustszAddress_1) AS customer_address_1,
+  INITCAP(CustszAddress_2) AS customer_address_2,
+  LocationszZipcode as customer_zipcode,
+  szLatitude AS address_latitude,
+  szLongitude AS address_longitude,
+  szSalesId as sales_id,
+  szSalesNm as sales_name,
+  DATE(PARSE_DATETIME("%d-%b-%Y %H:%M AM", dtmStart)) AS contract_begin_at
+FROM `logee-data-prod.stg_sheets.store_cirebon`)
+
+SELECT
+  *,
+  CURRENT_TIMESTAMP() AS published_timestamp
+FROM
+  base
