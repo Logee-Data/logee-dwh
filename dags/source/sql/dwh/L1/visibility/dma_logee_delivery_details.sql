82ef79d4b8c134aeadf882c5836eb7db724ff274
Fikri Haykal <fikri.haykal@telkom.co.id>
2022-07-25 14:01:26 +0700

    add new column in destinationList

--- dags/source/sql/dwh/L1/visibility/dma_logee_delivery_details.sql
+++ dags/source/sql/dwh/L1/visibility/dma_logee_delivery_details.sql
@@ -49,7 +49,14 @@ WITH base AS (
         CAST(IF(JSON_EXTRACT_SCALAR(destination_list, '$.destinationLatitude') = "", NULL, JSON_EXTRACT_SCALAR(destination_list, '$.destinationLatitude')) AS FLOAT64) AS destination_latitude,
         CAST(IF(JSON_EXTRACT_SCALAR(destination_list, '$.destinationLongitude') = "", NULL, JSON_EXTRACT_SCALAR(destination_list, '$.destinationLongitude')) AS FLOAT64) AS destination_longitude,
         IF(JSON_EXTRACT_SCALAR(destination_list, '$.destinationLocation') = "", NULL, JSON_EXTRACT_SCALAR(destination_list, '$.destinationLocation')) AS destination_location,
-        IF(JSON_EXTRACT_SCALAR(destination_list, '$.destinationPicPhone') = "", NULL, JSON_EXTRACT_SCALAR(destination_list, '$.destinationPicPhone')) AS destination_pic_phone
+        IF(JSON_EXTRACT_SCALAR(destination_list, '$.destinationPicPhone') = "", NULL, JSON_EXTRACT_SCALAR(destination_list, '$.destinationPicPhone')) AS destination_pic_phone,
+        CAST(IF(REPLACE(JSON_EXTRACT(destination_list, '$.destinationArrivedTime'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(destination_list, '$.destinationArrivedTime'), '"', '')) AS TIMESTAMP) AS destination_arrived_time,
+        IF(REPLACE(JSON_EXTRACT(destination_list, '$.destinationCargoImage'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(destination_list, '$.destinationCargoImage'), '"', '')) AS destination_cargo_image,
+        CAST(IF(REPLACE(JSON_EXTRACT(destination_list, '$.destinationCargoImageDate'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(destination_list, '$.destinationCargoImageDate'), '"', '')) AS TIMESTAMP) AS destination_cargo_image_date,
+        IF(REPLACE(JSON_EXTRACT(destination_list, '$.driverNote'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(destination_list, '$.driverNote'), '"', '')) AS driver_note,
+        IF(REPLACE(JSON_EXTRACT(destination_list, '$.reviewedDeliveryOrderFile'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(destination_list, '$.reviewedDeliveryOrderFile'), '"', '')) AS reviewed_delivery_order_file,
+        CAST(IF(REPLACE(JSON_EXTRACT(destination_list, '$.reviewedDeliveryOrderFileDate'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(destination_list, '$.reviewedDeliveryOrderFileDate'), '"', '')) AS TIMESTAMP) AS reviewed_delivery_order_file_date,
+        IF(REPLACE(JSON_EXTRACT(destination_list, '$.assignmentLetterFile'), '"', '') = "", NULL, REPLACE(JSON_EXTRACT(destination_list, '$.assignmentLetterFile'), '"', '')) AS assignment_letter_file
       )
     ) AS destination_list
   FROM
@@ -151,4 +158,3 @@ FROM
   LEFT JOIN delivery_detail_status C
   ON A.data = C.data
   AND A.ts = C.published_timestamp
-
