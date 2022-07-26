fb4ed716c1c5a673646ad267474803d35ba6ffa9
Yusuf Fahry <33114786+toileto@users.noreply.github.com>
2022-07-26 11:10:49 +0700

    Update query

--- dags/source/sql/dwh/L1/lgd_general/tx_cirebon_delivery.sql
+++ dags/source/sql/dwh/L1/lgd_general/tx_cirebon_delivery.sql
@@ -1,41 +1,41 @@
 WITH base AS (
   SELECT
-    DATE(PARSE_DATETIME("%d/%m/%Y %k:%M", tanggal_so)) AS so_date,
-    DATE(PARSE_DATETIME("%d/%m/%Y %k:%M", tanggal_rencana_kirim)) AS planned_send_date,
-    DATE(PARSE_DATETIME("%d/%m/%Y %k:%M", tanggal_terkirim)) AS received_date,
-    replace(id_cust,'NRE1','') as id_customer,
-    id_so as sales_order_id,
-    id_do as invoice_id,
-    `logee-data-prod.stg_sheets.tx_cirebon_ngirim`.id_produk as product_id,
+    DATE(tgl_order) AS so_date_wib,
+    DATE(tgl_rencana_kirim) AS planned_send_date_wib,
+    DATE(tgl_terkirim) AS received_date_wib,
+    replace(_cust,'NRE1','') as id_customer,
+    _so as sales_order_id,
+    _do as invoice_id,
+    `logee-data-prod.stg_sheets.tx_cirebon_ngirim`._prod as product_id,
     `logee-data-prod.stg_sheets.tx_cirebon_ngirim`.nama_produk as product_name,
-    CAST(qty_so AS INT64) AS so_quantity,
-    CAST(REPLACE(amount_so, ',', '.') AS FLOAT64) AS so_amount,
+    CAST(so_qty AS INT64) AS so_quantity,
+    GTV__rp_ AS so_amount,
     CAST(qty_do AS INT64) AS do_quantity,
-    CAST(REPLACE(amount_do, ',', '.') AS FLOAT64) AS do_amount,
+    amount_do AS do_amount,
   FROM `logee-data-prod.stg_sheets.tx_cirebon_ngirim`
 )
 
 , with_ppn AS (
   SELECT
     *,
-    ROUND(IF(so_date < '2022-04-01', so_amounT * 1.1, so_amount * 1.11), 2) AS so_amount_with_ppn,
-    ROUND(IF(so_date < '2022-04-01', do_amounT * 1.1, do_amount * 1.11), 2) AS do_amount_with_ppn,
-    (((long*width*height)/qty_ctn)*so_quantity)/1000 as so_dimension
+    ROUND(IF(so_date_wib < '2022-04-01', so_amounT * 1.1, so_amount * 1.11), 2) AS so_amount_with_ppn,
+    ROUND(IF(so_date_wib < '2022-04-01', do_amounT * 1.1, do_amount * 1.11), 2) AS do_amount_with_ppn,
+    (((p*l*t)/qty_karton)*so_quantity)/1000 as so_dimension_in_litres
   FROM
     base join `logee-data-prod.stg_sheets.product_dimension` on base.product_name=`logee-data-prod.stg_sheets.product_dimension`.nama_produk
 )
 
 ,stg AS (
   SELECT
-    so_date,
-    planned_send_date,
-    received_date,
-    DATE_DIFF(received_date,planned_send_date,DAY) as received_day_lag,
+    so_date_wib,
+    planned_send_date_wib,
+    received_date_wib,
+    DATE_DIFF(received_date_wib,planned_send_date_wib,DAY) as received_day_lag,
     id_customer,
     sales_order_id,
     invoice_id,
     -- dense_rank() OVER(PARTITION BY id_customer ORDER BY sales_order_id) purchase_order,
-    ROW_NUMBER() OVER(PARTITION BY id_customer ORDER BY date(so_date)) AS purchase_order_ordinal,
+    ROW_NUMBER() OVER(PARTITION BY id_customer ORDER BY date(so_date_wib)) AS purchase_order_ordinal,
     ARRAY_AGG(
       STRUCT(
         product_id,
@@ -58,11 +58,11 @@ WITH base AS (
     -- array_agg(do_amount_with_ppn) as do_amount_with_ppn,
     sum(so_amount_with_ppn) as sum_so_amount_with_ppn,
     sum(do_amount_with_ppn) as sum_do_amount_with_ppn,
-    sum(so_dimension) as so_dimension
+    sum(so_dimension_in_litres) as so_dimension
   FROM with_ppn group by
-    so_date,
-    planned_send_date,
-    received_date,
+    so_date_wib,
+    planned_send_date_wib,
+    received_date_wib,
     id_customer,
     sales_order_id,
     invoice_id
