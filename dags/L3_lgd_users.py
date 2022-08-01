a393c9082ed58dea17d7fed64a587fcde62a1e9d
Fikri Haykal <fikri.haykal@telkom.co.id>
2022-08-01 14:40:28 +0700

    Rename file and adjust airflow tasks

--- dags/L3_lgd_users.py
+++ dags/L3_lgd_users.py
@@ -35,7 +35,7 @@ default_args = {
 }
 
 dag = DAG(
-    dag_id='L3_lgd_fact_users',
+    dag_id='L3_lgd_users',
     schedule_interval='0 */3 * * *',
     default_args=default_args,
     catchup=False
